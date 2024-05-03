681f9d10262588b573ed15431a8d20189077d0df
loboale <alexisaragon79@gmail.com>
2024-05-03 01:17:41 -0300

    Agregue archivo Js

---
+++ static/Js/main.js
@@ -0,0 +1,8 @@
+// variables
+
+let nav = document.getElementById('nav');
+let menu = document.getElementById('enlaces');
+
+window.addEventListener('scroll', function(){
+    console.log(window.scrollX);
+});
